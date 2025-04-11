a9c30a7cd28024420298b17133ff3fa4b3562a9a
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2025-04-11 16:23:29 +0200

    [db_creation] add create/read/update/detele file, make start up endpoint in main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
+from .db import engine, yield_db
+from .model import Base
+from .fetch_xlsx import parse_and_load_xlsx
 
 app = FastAPI()
+Base.metadata.create_all(bind=engine)
+
+@app.on_event("startup")
+def startup():
+    print("Starting up app...")
+
+    # next because it's a generator
+    db = next(yield_db())
+    parse_and_load_xlsx(db, "Interns_2025_SWIFT_CODES.xlsx")
+    print("Database connected")
+
+    # TODO : add check if db is empty, if not then load data
 
 @app.get("/")
 def home():
