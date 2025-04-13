9f1f24d297970d6fa41f04cdfe29854345ee93a7
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2025-04-13 18:29:39 +0200

    [pytests] add test GET countryiso2code

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -15,6 +15,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 with SessionLocal() as db:
     swift_code_list: List[str] = [row[0] for row in db.query(UserSwiftDB.swift_code).all()]
+    iso2_list: List[str] = [row[0] for row in db.query(UserSwiftDB.country_iso2).distinct().all()]
 
 
 @pytest.mark.asyncio
@@ -31,3 +32,15 @@ async def test_get_swift_code_with_branches(swift_code):
         assert isinstance(data.get("countryISO2"), str)
         assert isinstance(data.get("countryName"), str)
         assert isinstance(data.get("isHeadquarter"), bool)
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("iso2_code", iso2_list)
+async def test_get_iso2_code(iso2_code):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"{BASE_URL}/v1/swift-codes/country/{iso2_code}")
+        assert response.status_code == 200, f"Niepoprawny status {response.status_code}"
+        
+        data = response.json()
+
+        assert data["countryISO2"] == iso2_code.upper()
+        # TODO: add more assertions
