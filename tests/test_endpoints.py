d7964b9820bfa768dedd739d2361cc6a0711f9bb
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2025-04-14 08:28:03 +0200

    [pytests] add test POST new data

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -38,9 +38,42 @@ async def test_get_swift_code_with_branches(swift_code):
 async def test_get_iso2_code(iso2_code):
     async with httpx.AsyncClient() as client:
         response = await client.get(f"{BASE_URL}/v1/swift-codes/country/{iso2_code}")
-        assert response.status_code == 200, f"Niepoprawny status {response.status_code}"
+        assert response.status_code == 200, f"Got {response.status_code}: {response.text}"
         
         data = response.json()
 
         assert data["countryISO2"] == iso2_code.upper()
         # TODO: add more assertions
+
+@pytest.mark.asyncio
+async def test_create_swift_code():
+    new_data = {
+        "swift_code": "FAKECODE123",
+        "bank_name": "Fake Bank",
+        "address": "123 Fake St",
+        "country_iso2": "FB",
+        "country_name": "FakeCountry",
+        "is_headquarter": True
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(f"{BASE_URL}/v1/swift-codes", json=new_data)
+        assert response.status_code == 200
+        data = response.json()
+        assert "message" in data
+        assert "FAKECODE123" in data["message"]
+
+        get_response = await client.get(f"{BASE_URL}/v1/swift-codes/{new_data['swift_code']}")
+        assert get_response.status_code == 200
+        get_data = get_response.json()
+        assert get_data["swiftCode"] == "FAKECODE123"
+        assert get_data["bankName"] == "Fake Bank"
+        assert get_data["address"] == "123 Fake St"
+        assert get_data["countryISO2"] == "FB"
+        assert get_data["countryName"] == "FakeCountry"
+        assert get_data["isHeadquarter"] == True
+
+
+# TODO: add POST duplicate test
+
+# TODO: add DELETE test
